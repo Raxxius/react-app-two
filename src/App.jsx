a68eb736bc5d5925bd45ecff0af85c8850404c08
Raxxius <raxxman@gmail.com>
2024-01-25 18:34:49 +0000

    reduce to core return

--- src/App.jsx
+++ src/App.jsx
@@ -2,34 +2,34 @@ import { useState, useEffect } from 'react'
 import './App.css'
 
 const App = () => {
-  const [currentColourIndex, setCurrentColourIndex] = useState(0);
-  const [colours, setColours] = useState("")
+  // const [currentColourIndex, setCurrentColourIndex] = useState(0);
+  // const [colours, setColours] = useState("")
 
-  console.log("App two session storage is " + sessionStorage)
+  // console.log("App two session storage is " + sessionStorage)
 
-  useEffect(() => {
-    // Fetch colors from sessionStorage on component mount
-    const storedColours = sessionStorage.coloursTable;
-    setColours(storedColours);
-    console.log(storedColours)
-  }, []);
+  // useEffect(() => {
+  //   // Fetch colors from sessionStorage on component mount
+  //   const storedColours = sessionStorage.coloursTable;
+  //   setColours(storedColours);
+  //   console.log(storedColours)
+  // }, []);
 
-  const handleNextColor = () => {
-    const nextIndex = (currentColourIndex + 1) % colours.length;
-    setCurrentColourIndex(nextIndex);
-    updateSessionStorage(colours[nextIndex]);
-  };
+  // const handleNextColor = () => {
+  //   const nextIndex = (currentColourIndex + 1) % colours.length;
+  //   setCurrentColourIndex(nextIndex);
+  //   updateSessionStorage(colours[nextIndex]);
+  // };
 
-  const handlePrevColor = () => {
-    const prevIndex = (currentColourIndex - 1 + colours.length) % colours.length;
-    setCurrentColourIndex(prevIndex);
+  // const handlePrevColor = () => {
+  //   const prevIndex = (currentColourIndex - 1 + colours.length) % colours.length;
+  //   setCurrentColourIndex(prevIndex);
 
-    updateSessionStorage(colours[prevIndex]);
-  };
+  //   updateSessionStorage(colours[prevIndex]);
+  // };
 
-  const updateSessionStorage = (newColour) => {
-    sessionStorage.setItem('currentColour', newColour);
-  };
+  // const updateSessionStorage = (newColour) => {
+  //   sessionStorage.setItem('currentColour', newColour);
+  // };
 
 
   return (
